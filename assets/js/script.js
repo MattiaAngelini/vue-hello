acd93d3c1ebb343d288168cde71d3e0e97966087
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-26 18:22:39 +0100

    Add: message in h1 - Milestone 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
-let mattia = 0;
-console.log(mattia);
+//Descrizione:
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            introMessage: 'Benvenuto!'           
+        };
+    }
+}).mount('#app');
